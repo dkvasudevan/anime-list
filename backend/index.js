00faa9a4c6168c5be472a3ad11f592c28393bda4
Dan Vasudevan <dkvasudevan@Dans-MacBook-Air.local>
2024-08-24 18:23:30 -0700

    adding app

---
+++ backend/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const colors = require("colors");
+
+dotenv.config();
+colors.enable();
+
+const Anim = require("./database/anim.model");
+const connect = require("./database/connect");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log("Hello World!".rainbow);
+
+  res.send("Hello World!");
+});
+
+app.get("/api/anime", async (req, res) => {
+    anime = await Anim.find();
+  res.json(anime);
+});
+
+app.post("/api/anime", async (req, res) => {
+  const anime = new Anim(req.body);
+  await anime.save();
+  res.json(anime);
+});
+
+app.delete("/api/anime",async (req, res) => {
+  console.log("DELETE");
+});
+
+app.listen(8000, () => {
+  console.log("server listening on port 8000");
+
+  // connect to the database
+  connect();
+});
